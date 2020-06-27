f2debbaa940688efb5409cecab1abde1c2c492a5
Denis <denisgarifulin@gmail.com>
2020-06-27 13:15:12 -0700

    added custome recipe details view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -44,4 +44,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """retrieve recipes for authenticated users"""
         return self.queryset.filter(user=self.request.user)
 
+    def get_serializer_class(self):
+        """return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
+
 
